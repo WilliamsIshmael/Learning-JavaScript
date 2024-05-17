da5b8a81e34b6208c5d19718842e282c07f5e911
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-05-16 21:47:36 -0400

    Create JavaScript Course Part 2.js

---
+++ JavaScript Course Part 2.js
@@ -0,0 +1,14 @@
+// FUNCTIONS 
+`A JavaScript function is a block of code designed to perform a particular task.
+
+A JavaScript function is executed when "something" invokes it (calls it).
+
+Start with the 'function' keyword then follow with the name of the function.`
+
+function speaker() {
+  console.log('Hello, I am the speaker')
+}
+
+speaker(); 
+speaker(); 
+speaker(); 
