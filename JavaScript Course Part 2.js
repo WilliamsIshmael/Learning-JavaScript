510211547e13fb3d7d9deb9c6ac7da8cd0b11a59
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-07-02 11:03:03 -0400

    Update JavaScript Course Part 2.js

--- JavaScript Course Part 2.js
+++ JavaScript Course Part 2.js
@@ -65,6 +65,29 @@ contestant['location'] = "Miami, Florida";
 
 console.log(`${contestant.firstName} ${contestant.lastName} is a ${contestant.age} year old CrossFit athelete. He is from ${contestant.location} and is a member at ${contestant.homeGym}.`);
 
+const mark = { 
+  fullName: 'Mark Miller', 
+  mass: 78, 
+  height: 1.69,
+
+ calcBMI: function() {
+    return this.mass / Math.pow(this.height, 2);
+  }
+}; 
+
+const john = { 
+  fullName: 'John Smith', 
+  mass: 92, 
+  height: 1.95,
+
+   calcBMI: function() {
+    return this.mass / Math.pow(this.height, 2);
+  }
+}; 
+
+console.log(mark.calcBMI());
+console.log(john.calcBMI());
+
 
 
 
