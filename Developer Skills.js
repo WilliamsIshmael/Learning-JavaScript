e36446e2cd308c6c22bd251cfba61a310ee44404
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-07-05 21:09:08 -0400

    Create Developer Skills.js

---
+++ Developer Skills.js
@@ -0,0 +1,7 @@
+// Guidance on becoming a software developer
+
+// Have clear goals. 
+// Have systems in place to achieve the goals. 
+// Practice daily. 
+  // Leetcode and CodeWars are amazing places to start practicing. 
+// Always type code on tutorials, don't simply copy and paste. 
