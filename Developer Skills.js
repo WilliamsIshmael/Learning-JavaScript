e6c76cc68dc0cc27564180d85c327d7e7925767c
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-07-06 22:44:10 -0400

    Update Developer Skills.js

--- Developer Skills.js
+++ Developer Skills.js
@@ -5,3 +5,11 @@
 // Practice daily. 
   // Leetcode and CodeWars are amazing places to start practicing. 
 // Always type code on tutorials, don't simply copy and paste. 
+
+
+// How to think like a developer; become a problem solver. 
+
+// Understand the problem. 
+// Break the problem into smaller problems. 
+// Research when necessary. Don't hesitate. 
+// Write psuedo-code before writing actual code. 
