44ab7041c9640b3c35dae61177c525ac8dc21966
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-07-08 23:32:45 -0400

    Update CodeWars Kata Solutions.js

--- CodeWars Kata Solutions.js
+++ CodeWars Kata Solutions.js
@@ -58,6 +58,31 @@ function simpleMultiplication(number) {
       }
 }
 
+// Grasshopper - Basic Function Fixer 
+`
+I created this function to add five to any number that was passed in to it and return the new value. 
+It doesn't throw any errors but it returns the wrong number.
+Can you help me fix the function?
+`
+function addFive(num) {
+  var total = num + 5
+  return total;
+}
+
+// Grasshopper - Terminal game combat function 
+`
+Create a combat function that takes the player's current health and the amount of damage recieved, 
+and returns the player's new health. Health can't be less than 0.
+`
+function combat(health, damage) {
+  if (health - damage > 0) {
+    return (health - damage) 
+  } else {
+    return 0;
+  }  
+}
+
+
 
 
 
