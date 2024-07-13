1c8a221640fc508dffbdbea8d60a0f0a2e1f483f
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-07-12 23:33:32 -0400

    Update CodeWars Kata Solutions.js

--- CodeWars Kata Solutions.js
+++ CodeWars Kata Solutions.js
@@ -101,5 +101,18 @@ function makeUpperCase(str) {
   return str.toUpperCase();
 }
 
+// Convert a Number to a String!
+`
+We need a function that can transform a number (integer) into a string.
 
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+`
+function numberToString(num) {
+  return num.toString();
+}
 
