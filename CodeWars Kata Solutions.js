fc0c8bf02094f2c712b411c3ff154a30bd461b1a
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-07-18 18:43:09 -0400

    Update CodeWars Kata Solutions.js

--- CodeWars Kata Solutions.js
+++ CodeWars Kata Solutions.js
@@ -117,8 +117,25 @@ function numberToString(num) {
 }
 
 // Convert Age to Days 
-`Create a function that takes the age in years and returns the age in days.
+`
+Create a function that takes the age in years and returns the age in days.
 ` 
 function calcAge(age) {
 	return age * 365;
+}
+
+// Grasshopper - Messi goals function
+`
+Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+`
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return (laLigaGoals + copaDelReyGoals + championsLeagueGoals);
 }
