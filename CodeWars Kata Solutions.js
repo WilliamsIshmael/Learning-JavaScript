6f3d0e78e24249d7f1646b996de779e9d176e026
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-07-30 22:42:25 -0400

    Update CodeWars Kata Solutions.js

--- CodeWars Kata Solutions.js
+++ CodeWars Kata Solutions.js
@@ -156,6 +156,15 @@ function past(h, m, s){
 Make a simple function called greet that returns the most-famous "hello world!".
 `
 
+// Convert boolean values to strings 'Yes' or 'No'
+` 
+Complete the method that takes a boolean value and return a "Yes" string for true, 
+or a "No" string for false.
+`
+function boolToWord( bool ){
+  if (bool) return "Yes"
+  else return "No"; 
+  }
 function greet() {
   return 'hello world!'; 
   }
